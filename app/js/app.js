4176ccd90756e745de6e460f0a183ec8fd3929cc
Anthony <ach0074@gmail.com>
2020-10-22 15:02:43 -0400

    Updated app.js with event listeners

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,20 @@
 import '../scss/styles.scss'
 
-console.log('Hello world!')
+const btns = document.querySelectorAll('.btn')
+const inputs = document.querySelectorAll('form input')
+
+// Only clear when input is no longer active to prevent overlap with label
+inputs.forEach(input => input.addEventListener('blur', focusOutInputClear))
+
+btns.forEach(btn => btn.addEventListener('click', e => {
+  e.preventDefault()
+
+  const forms = document.querySelectorAll('form')
+  forms.forEach(form => form.reset())
+  
+  // console.log('Form has been reset')
+}))
+
+function focusOutInputClear() {
+  this.value = ''
+}
