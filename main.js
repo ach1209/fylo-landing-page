d9af4b03b06863260f44bc68c167a86aa5f74bdd
Anthony <ach0074@gmail.com>
2023-03-14 11:21:48 -0400

    Removed unused css file, moved old js file into main.js, fixed input accessibility issue

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './styles.scss'
+
+const btns = document.querySelectorAll('button')
+
+btns.forEach(btn => btn.addEventListener('click', e => {
+  e.preventDefault()
+
+  const forms = document.querySelectorAll('form')
+  forms.forEach(form => form.reset())
+}))
+
+const inputs = document.querySelectorAll('form input')
+inputs.forEach(input => input.addEventListener('blur', focusOutInputClear))
+
+function focusOutInputClear() {
+  this.value = ''
+}
